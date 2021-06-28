a79f42a2b669f261786cdea9a849bdefd8ebfa0a
Ростислав <kingartes1989@gmail.com>
2021-06-28 16:46:32 +0300

    Initial structure

---
+++ Assets/Slicer.cs
@@ -0,0 +1,28 @@
+﻿using BzKovSoft.ObjectSlicerSamples;
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slicer : MonoBehaviour
+{
+    [SerializeField] private GameObject _blade;
+    [SerializeField] private float _duration;
+    [SerializeField] private float _offsetY;
+
+    private BzKnife _knife;
+
+    private void Start()
+    {
+        _knife = _blade.GetComponentInChildren<BzKnife>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _knife.BeginNewSlice();
+            _blade.transform.DOMoveY(_blade.transform.position.y - _offsetY, _duration).SetLoops(2, LoopType.Yoyo);
+        }
+    }
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _templates;
+    [SerializeField] private float _spawnDelay;
+
+    private void Start()
+    {
+        StartCoroutine(Spawm());
+    }
+
+    private IEnumerator Spawm()
+    {
+        while (true)
+        {
+            int foodNumber = Random.Range(0, _templates.Length);
+            GameObject food = Instantiate(_templates[foodNumber], transform.position, _templates[foodNumber].transform.rotation);
+            yield return new WaitForSeconds(_spawnDelay);
+        }
+    }
+}
