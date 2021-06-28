ff35e78bbda21bb6dc48c36ca47031d0bd7cd9f4
Ростислав <kingartes1989@gmail.com>
2021-06-28 17:35:09 +0300

    Add points counter

---
+++ Assets/PointsCounter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointsCounter : MonoBehaviour
+{
+    private int totalPoints;
+
+    public void AddPoints(int pointsAmount)
+    {
+        totalPoints += pointsAmount;
+        Debug.Log(totalPoints);
+    }
+}
